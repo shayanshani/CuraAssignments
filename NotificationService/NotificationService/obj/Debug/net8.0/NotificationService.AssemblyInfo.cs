db8fcefc0657c1e97c37562a8b75ba70608a14ed
Shayan Hafeez <shayanhafeez@gmail.com>
2024-06-08 12:39:38 +0500

    Removing

--- NotificationService/NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
+++ NotificationService/NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74d04f6b0ba0b8cd3ad4dc1cbc75e0df19cd4aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f312590ff415a7ca75277b2401998cada6d7846")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
